1b2b4ab97c6653e939416168678da75a593ac04d
junyeongyu <Junyeong.Yu@MyGeorgian.ca>
2017-02-22 11:52:13 -0500

    midterm test commit

--- app.js
+++ app.js
@@ -27,29 +27,10 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', require('./routes/index'));
 app.use('/players', require('./routes/players'));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-app.listen(4000, function () {
-  console.log('Example app listening on port 4000!');
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!!!!!');
 });
 
 module.exports = app;
